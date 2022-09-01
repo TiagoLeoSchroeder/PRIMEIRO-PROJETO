a8dd3c304c4eafc0338d1041b1266a86d7799e0e
Tiago Leonardo Schroeder <111138016+TiagoLeoSchroeder@users.noreply.github.com>
2022-09-01 11:21:05 -0300

    Add files via upload

---
+++ conexao.php
@@ -0,0 +1,12 @@
+<?php 
+
+$hostname = "localhost";
+$bancodedados = "SENAI";
+$usuario = "root";
+$senha = "";
+
+$mysqli = new mysqli($hostname,$bancodedados,$usuario,$senha);
+if ($mysqli->connect_error) {
+    echo "Falha ao conectar: (" . $mysqli->connect_error . ") " . $mysqli->connect_error;
+} else
+    echo "Conectado!";
