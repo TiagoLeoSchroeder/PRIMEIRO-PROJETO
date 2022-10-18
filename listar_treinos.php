003a8b5643dcc371ae137caab5d010fe31075efd
Tiago Leonardo Schroeder <111138016+TiagoLeoSchroeder@users.noreply.github.com>
2022-10-18 12:10:17 -0300

    update

--- listar_treinos.php
+++ listar_treinos.php
@@ -1,6 +1,8 @@
 <?php
     include("conexao.php");
 
-    
+    $comando = $pdo->prepare("SELECT nome_treino, nivel_treino, equipamento_treino, musculo_treino FROM treino");
+
+    $comando->execute();
 
 ?>
