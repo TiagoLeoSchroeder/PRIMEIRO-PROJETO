2c1a16c7cc725e4e19385e90aff765c916dd7163
Tiago Leonardo Schroeder <111138016+TiagoLeoSchroeder@users.noreply.github.com>
2022-10-21 11:40:11 -0300

    update

--- tela_cadastro_equipamento.php
+++ tela_cadastro_equipamento.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="StyleSheet" href="estilo_cadastro_equipamento.css">
+    <link rel="StyleSheet" href="estilo_equipamento.css">
     <title>Tela de cadastro de equipamento</title>
 </head>
 <body>
