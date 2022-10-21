160c1851e697707274e90e65ecd8ebb943fd5d3e
Tiago Leonardo Schroeder <111138016+TiagoLeoSchroeder@users.noreply.github.com>
2022-10-21 12:28:10 -0300

    update

--- tela_cadastro_equipamento.php
+++ tela_cadastro_equipamento.php
@@ -12,13 +12,14 @@
     <title>Tela de cadastro de equipamento</title>
 </head>
 <body>
-    <h1 style="position: relative; left: 30%;">CADASTRO DE EQUIPAMENTOS</h1>
+    <h1 class="textocima">CADASTRO DE EQUIPAMENTOS</h1>
     <form action="cadastrar_equipamento.php" method="post">
+        <h1></h1>
         <label for="Nome"></label>
         <input type="text" name="Nome" placeholder="Nome do Equipamento: ">
         <br>
         <label for="descricao"></label>
-        <input type="textarea" name="descricao" placeholder="Descrição: ">
+        <textarea name="descricao" placeholder="Descrição: "></textarea>
         <br>
         <input type="submit" value="enviar">
 </form>
