e214da446dc6cb48c68361ea98ca77f96eeabe28
Tiago Leonardo Schroeder <111138016+TiagoLeoSchroeder@users.noreply.github.com>
2022-11-01 07:50:25 -0300

    update

--- tela_cadastro_treinos.php
+++ tela_cadastro_treinos.php
@@ -1,13 +1,11 @@
-<?php 
-    include('conexao.php');
-?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" href="estilo_cadastro_treino.css">
+    <link rel="stylesheet" href="estilo_cadastro_treinamento.css">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Tela Cadastro de Treinos</title>
 </head>
@@ -15,13 +13,18 @@
 
     <form action="cadastrar_treino.php" method="post">
     <p>CADASTRAR TREINOS🏋🏽</p>
-        <input type="text" placeholder="Nome do Treino">
+        <input type="text" placeholder="Nome do Treino" name="nome" id="nome">
         <br>
-        <textarea placeholder="Descrição"></textarea>
+        <textarea placeholder="Descrição" name="descricao" id="descricao"></textarea>
         <br>
         <input type="submit">
-        <input type="button" value="Voltar">
+        <input type="button" value="Voltar" onclick="Voltar()">
     </form>
-</div>
+<script>
+    function Voltar(){
+        window.close('tela_cadastro_treinos.php');
+        window.open('tela_inicio_admin.php');
+    }
+</script>
 </body>
 </html>
