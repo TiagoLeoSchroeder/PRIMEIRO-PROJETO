6fdd1ecfde96b6fcddfb1f029b07d298e95feb96
Tiago Leonardo Schroeder <111138016+TiagoLeoSchroeder@users.noreply.github.com>
2022-11-01 08:10:31 -0300

    update

--- cadastrar_treino.php
+++ cadastrar_treino.php
@@ -1,19 +1,20 @@
 <?php
+// include: ele inclui e avalia o arquivo informado. //
     include('conexao.php');
-
+//------------------------------------------//
     $nome = $_POST['nome'];
-
+//------------------------------------------//
     $descricao = $_POST['descricao'];
-
+//------------------------------------------//
     $comando = $pdo->prepare("INSERT INTO treinos(nome_treino, descricao_treino) VALUES(:nome,:descricao)");
-
+//------------------------------------------//
     $comando->bindValue(":nome",$nome);
     $comando->bindValue(":descricao",$descricao);
-
+//------------------------------------------//
     $comando->execute();
-
+//------------------------------------------//
     header("Location:tela_cadastro_treinos.php");
-
+//------------------------------------------//
     unset($comando);
     unset($pdo);
 ?>
--- conexao.php
+++ conexao.php
@@ -4,6 +4,7 @@
     
     // CONEXÃO COM A BASE DE DADOS: verifique sempre o dbname
     try{
+        // PDO: define uma interface de conexão a banco de dados leve e consistente para PHP.
         $pdo = new PDO("mysql:dbname=academia;host=localhost;charset=utf8","root","");
     }
     catch(PDOException $erro)
